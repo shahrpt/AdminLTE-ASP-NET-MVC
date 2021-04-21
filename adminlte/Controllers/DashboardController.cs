be3f416aec1786de0a6cf93f6fd4bc04c675e900
Sulman <shahsuliman@gmail.com>
2021-04-21 19:24:43 +0500

    added deveexpress support and the data gridview

--- adminlte/Controllers/DashboardController.cs
+++ adminlte/Controllers/DashboardController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DevExpress.Web.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -14,6 +15,10 @@ namespace adminlte.Controllers
             return View();
         }
 
+        public ActionResult Products()
+        {
+            return View();
+        }
         public ActionResult Dashboardv1()
         {
             return View();
@@ -22,6 +27,83 @@ namespace adminlte.Controllers
         public ActionResult Dashboardv2()
         {
             return View();
+        }
+
+
+        adminlte.Models.CartRoverEntities db = new adminlte.Models.CartRoverEntities();
+
+        [ValidateInput(false)]
+        public ActionResult GridView1Partial()
+        {
+            var model = db.Products;
+            
+            return PartialView("_GridView1Partial", model.ToList());
+        }
+
+        [HttpPost, ValidateInput(false)]
+        public ActionResult GridView1PartialAddNew([ModelBinder(typeof(DevExpressEditorsBinder))] adminlte.Models.Product item)
+        {
+            var model = db.Products;
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    model.Add(item);
+                    db.SaveChanges();
+                }
+                catch (Exception e)
+                {
+                    ViewData["EditError"] = e.Message;
+                }
+            }
+            else
+                ViewData["EditError"] = "Please, correct all errors.";
+            return PartialView("_GridView1Partial", model.ToList());
+        }
+        [HttpPost, ValidateInput(false)]
+        public ActionResult GridView1PartialUpdate([ModelBinder(typeof(DevExpressEditorsBinder))] adminlte.Models.Product item)
+        {
+            var model = db.Products;
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    
+                    var modelItem = model.FirstOrDefault(it => it.PID == item.PID);
+                    if (modelItem != null)
+                    {
+                        this.TryUpdateModel(modelItem);
+                        db.SaveChanges();
+                    }
+                }
+                catch (Exception e)
+                {
+                    ViewData["EditError"] = e.Message;
+                }
+            }
+            else
+                ViewData["EditError"] = "Please, correct all errors.";
+            return PartialView("_GridView1Partial", model.ToList());
+        }
+        [HttpPost, ValidateInput(false)]
+        public ActionResult GridView1PartialDelete(System.Int32 PID)
+        {
+            var model = db.Products;
+            if (PID >= 0)
+            {
+                try
+                {
+                    var item = model.FirstOrDefault(it => it.PID == PID);
+                    if (item != null)
+                        model.Remove(item);
+                    db.SaveChanges();
+                }
+                catch (Exception e)
+                {
+                    ViewData["EditError"] = e.Message;
+                }
+            }
+            return PartialView("_GridView1Partial", model.ToList());
         }
     }
 }
---
+++ adminlte/Service/Common.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace adminlte.Service
+{
+    
+    public static class CommonClient
+    {
+        //adminlte.Models.CartRoverEntities db = new adminlte.Models.CartRoverEntities();
+
+        /*public static IEnumerable GetBrands()
+        {
+            var brands = from brand in db.Brand
+                        select new
+                        {
+                            BrandId = brand.BID,
+                            BrandName = brand.BrandName,
+                           
+                        };
+            return brands.ToList();
+        }*/
+    }
+}
